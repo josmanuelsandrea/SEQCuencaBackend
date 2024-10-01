913f97d929b5a44ce7ba1d4d89d0e94efb6f7573
Josmanuel Sandrea <79341120+josmanuelsandrea@users.noreply.github.com>
2024-10-01 12:07:25 -0500

    Updated VehicleEditRequest because fleetnumber was not being updated

--- ScaneqCuencaBackend/Models/RequestModels/VehicleEditRequest.cs
+++ ScaneqCuencaBackend/Models/RequestModels/VehicleEditRequest.cs
@@ -15,6 +15,7 @@ namespace ScaneqCuencaBackend.Models.RequestModels
         public decimal? RearAxleGearRatio { get; set; }
         public string Plate { get; set; } = null!;
         public string? Type { get; set; }
+        public int? FleetNumber { get; set; } = null!;
         public bool? MaintenanceAgreement { get; set; }
         public int? CooperativeId { get; set; }
     }
